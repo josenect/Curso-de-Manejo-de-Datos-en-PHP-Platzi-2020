f030e25758081b2f3b2ab783b683fde0eca75b2a
Jose Rodriguez <josenect@hotmail.es>
2020-12-09 20:03:30 -0500

    se verifico las diferencia y casos en los cuales se utiliza las comillas dobles y las comillas simples al igual el manejo de {} para impresion de datos complejos adicional mente se analiso el manejo de variables variables

---
+++ index.php
@@ -0,0 +1,67 @@
+    <?php
+
+                // 1. Capitulo Comillas 
+
+/**
+ *    carateres a rescatar  (',\)
+ *    ' no se conoce la ' que cierra la impresion  solucion \ antes de la '
+ * 
+ *    \ por que espera un caracter especial solucion agregar \  final queda \\ 
+ * 
+ * ejemplo
+ * 
+ */
+
+   echo 'Un texto de una linea  \' varias lineas   \\ Continuo imprimiendo texto <br>';
+
+/*
+ *  Comillas simples 
+ * se debe concatenar con . para realizar la impresion de variables esto hace que el codigo sea mas extenso y sucio.
+ * 
+ *  */
+
+$nombre = 'Jose Rodriguez';
+
+echo 'Mi Nombre es ' . $nombre . ' Final <br>';
+
+/*
+ * comillas dobles -- 
+ * con este tipo de impresion de " " no se debe de utilizar . para realizar la concatenacion de las variables
+*/
+
+$apellido = 'Rodriguez Bonilla';
+
+echo "Mi Apellido es  $apellido  Final <br>";
+
+
+/*
+ * 
+ * 
+ * impresion de datos complejos se acompañan de {}
+ * 
+ */
+$courses = [
+
+    'backend' => ['PHP','Laravel']
+];
+
+echo "{$courses['backend'][0]} <br>";
+
+/*
+ * 
+ * 
+ * analisis de variables variables
+ * 
+ */
+
+$alumno ='jose';
+$jose = 'Estudiante de PHP';
+
+echo "$alumno es ${$alumno}";
+
+
+
+
+
+
+
