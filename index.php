0bf66165dffc8ad761a0c485fae6d047ef2f29f5
Jose Rodriguez <josenect@hotmail.es>
2021-01-14 19:13:50 -0500

    se agregaron metodos de que s epuede aplicar para realizar accion con los array como busqueda impresion comparacion etc.

--- index.php
+++ index.php
@@ -147,3 +147,143 @@ $courses = ['JavaScript',
 
 echo "<pre>";
 var_dump($courses);
+
+
+/* Array complejo 
+
+*/
+
+
+$courses = [
+'frontend'=>'JavaScript',
+'Framework'=>'Laravel',
+'backend'=>'PHP'];
+
+echo "<pre>";
+var_dump($courses);
+
+foreach ($courses as $key => $value) {
+    # code...
+
+    echo "$key :$value <br>";
+}
+
+foreach ($courses as $course) {
+    # code...
+
+    echo "$course <br>";
+}
+echo " <br>";
+echo " funciones de array <br>";
+
+/* Array funciones dentro del array 
+
+walk - caminar aplicar una funcion a cada miembro de un array 
+
+array_key_exists('valor a buscar',$courses); retorna true o false , busqueda si esxiste ese key en es array.
+in_array('valor a buscar',$courses); returna valor boolean , busqueda si existe el valor en el array .
+array_keys(); impresion de todos los keys del array definido.
+array_values();impresion de todos los valores del array definido.
+*/
+var_dump(array_key_exists("frontend",$courses));
+var_dump( in_array("Laravel",$courses));
+var_dump(array_keys($courses));
+var_dump(array_values($courses));
+
+/* Funciones PHP para arrays
+
+sort: Ordena un array.
+rsort: Ordena un array en orden inverso.
+ksort: Ordena un array por clave.
+krsort: Ordena un array por clave en orden inverso.
+array_slice: Extrae una parte de un array.
+array_chunk: Divide un array en fragmentos.
+array_shift: Quita un elemento del principio del array.
+array_pop: Extrae el último elemento del final del array.
+array_unshift: Añadir al inicio de un array uno a más elementos.
+array_push: Inserta uno o más elementos al final de un array.
+array_flip: Intercambia todas las claves de un array con sus valores asociados.
+
+*/
+$courses = ['JavaScript',
+'Laravel',
+'PHP',
+'Vue.js'];
+
+
+/* Comparacion de Array  
+
+*/
+
+$courses = ['php', 'javascript'];
+$wishes = ['php', 'javascript', 'laravel', 'vuejs'];
+//Por ejemplo, tengo dos cursos(PHP y JS) pero quisiera saber que otros cursos le gustaría a mis estudiantes
+//Con esto comparo los array y descarto los cursos que yo ya tengo.
+
+var_dump(array_diff($wishes, $courses));
+
+
+echo "<br>";
+$arrayA = [1, 2, 3, 4, 5];
+$arrayB = [3, 4, 5, 6, 7];
+
+var_dump(array_diff($arrayA, $arrayB));
+
+/* union en array  
+
+*/
+
+
+
+$frontent = [
+	'javascript'
+];
+
+$backend = [
+	 'php', 
+	 'laravel'
+];
+
+echo "<pre>";
+
+//var_dump($frontent + $backend);
+
+
+echo "<pre>";
+var_dump(array_merge($frontent, $backend));
+
+$frontent3 = [
+	'a' => 'javascript'
+];
+
+$backend3 = [
+	'a' => 'php', 
+	'b' => 'laravel'
+];
+
+echo "<pre>";
+var_dump(array_merge_recursive($frontent3, $backend3));
+//Esto crea un array "a" donde mete unos elementos y otro "b" que mete los demás
+//resultado
+/* 
+array(2) {
+  ["a"]=>
+  array(2) {
+    [0]=>
+    string(10) "javascript"
+    [1]=>
+    string(3) "php"
+  }
+  ["b"]=>
+  string(7) "laravel"
+}
+*/
+
+echo "<br>";
+//Ejemplo con los mismo key
+#array_combine
+//Crea un nuevo array, usando una matriz para las claves y otra para sus valores
+$courses = ['javascript', 'php', 'laravel'];
+$categories = ['frontend', 'backend', 'framework'];
+
+var_dump(array_combine($categories, $courses));
